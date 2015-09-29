44a2eed206f451411c8bff4d9e147f1d3c5df880
joshrobo <joshuaed@hawaii.edu>
2015-09-28 17:59:28 -1000

    used args

--- throw_err.py
+++ throw_err.py
@@ -4,21 +4,13 @@
 # user-supplied command line argument
 
 import sys
+import argparse
 
-def print_usage():
-    """Print usage and exit"""
-    sys.stderr.write("usage: python raise_err.py <error type>\n")
-    sys.stderr.write("available errors: \n")
-    sys.stderr.write("\tassertion, io, import, index\n")
-    sys.stderr.write("\tkey, name, os, type, value,\n")
-    sys.stderr.write("\tzerodivision\n")
-    sys.exit()
-
-# Check args
-if len(sys.argv) != 2:
-    print_usage()
-
-error_type = sys.argv[1]
+available_errors = ["assertion","io","import","index","key","name", "os","type", "value", "zerodivision"]
+parser = argparse.ArgumentParser()
+parser.add_argument("error_type",choices = available_errors)
+args = parser.parse_args()
+error_type= args.error_type
 
 if error_type == "assertion":
     raise AssertionError
